8495440198cde1b0d6800c0638fdbd17c35eb619
ik <iank@iank.org>
2015-06-03 13:40:27 -0400

    Find (permutations of) tiles corresponding to each word

--- bestword.py
+++ bestword.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import itertools
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 grid = [
 {'i':0,'j':0,'team':'blue','letter':'','capital':0},
@@ -40,6 +43,11 @@ grid = [
 {'i':-3,'j':2,'team':'none','letter':'o','capital':0},
 {'i':-1,'j':-1,'team':'blue','letter':'','capital':0}];
 
+def get_tile(eye, jay):
+    for tile in grid:
+        if (tile['i'] == eye and tile['j'] == jay):
+            return tile
+
 ## Determine available letters
 letters = []
 for tile in grid:
@@ -80,7 +88,49 @@ for k,word in enumerate(possible_words[:-1]):
         redundant.append(word)
 
 possible_words = list(set(possible_words) - set(redundant))
- 
-#print letters
-#print letter_count
-print possible_words
+
+## Find tiles corresponding to each word- multiple permutations where possible
+solns = {}
+#for word in possible_words:
+word = possible_words[12]
+if 1==1:
+    letters = list(word)
+    ll = letter_count.copy()
+    tiles = []
+    for letter in letters:
+        # find the tile[s] corresponding to this
+        tiles.append([x for x in grid if x['letter'] == letter])
+
+    # Supposing there are 2 of one letter, and a word uses 2 of that letter, 
+    # product here gives me words that use letter 1 then letter 2,
+    # letter 2 then letter 1, letter 1 twice, and letter 2 twice. But all are
+    # equivalent for the game
+    candidates = list(itertools.product(*tiles));
+    solns = []
+    hashes = []
+    for candidate in candidates:
+        hashable = [(x['i'],x['j'])for x in candidate]
+        # First part: make sure we haven't used same coords twice
+        # Second part: order doesn't matter
+        if (len(candidate) == len(list(set(hashable))) and set(hashable) not in hashes):
+            hashes.append(set(hashable))
+            solns.append({'word': word, 'tiles':candidate})
+
+## Score each word
+for candidate in solns:
+    candidate['score'] = {
+        'length': len(list(word)),
+        'connected_length': 0,
+        'mycapital_adjacent': 0,
+        'enemycapital_adjacent': 0,
+        'blue_adjacent': 0,
+    }
+
+    # Determine connected length
+    # Start w/ all red tiles
+    #     Check 6 adjacent- if letter, 
+#    list = [tile in grid if tile['team'] == 'red']
+#    while (len(list) > 0):
+#        tile = list.pop(0)
+        
+    # pop(0) == unshift
